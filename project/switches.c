bd0a523ebb48963c9094e988520254bf84539d79
dapacheconunez <dapacheconunez@miners.utep.edu>
2017-04-06 17:42:11 -0600

    commenting

--- project/switches.c
+++ project/switches.c
@@ -3,9 +3,9 @@
 
 void switchInit(){
 
-  P2REN |= SWITCHES;
-  //P2IE = SWITCHES;
-  //P2OUT |= SWITCHES;
-  P2DIR &= ~SWITCHES;
+  P2REN |= SWITCHES;    //enable the switches on board P2
+  //P2IE = SWITCHES;    //diable interrupts, this makes changes "instantaneous". doesn't re-run main.
+  //P2OUT |= SWITCHES;  //switches won't be used as output.
+  P2DIR &= ~SWITCHES;   //switches will be read as input.
   
 }
